74e9f88650a6c69f4a74d92c8b60634e7efc6fc8
Emilio Puga <ea.pugaavalos@ugto.mx>
2021-04-21 12:11:24 -0500

    Agregar que filtre la pared por profundidad y que se le aplique la máscara al color cielab

--- kinectColorSeg.m
+++ kinectColorSeg.m
@@ -9,36 +9,44 @@ clear all;
 [u, v, ch] = size(imgColor);
 imgMask = zeros(u,v);
 
+% Usar HSV para segmentar
 bwDepth = rgb2hsv(imgColor);
 bwDepth = imadjust(bwDepth,[],[],1.2);
 
-%se = strel('disk',50);
-%bwDepth = imtophat(bwDepth, se);
 lv = 5;
 channel = 2;
+paredMin = 160;
 
 th = multithresh(bwDepth(:,:,channel), lv);
 imSeg = imquantize(bwDepth(:,:,channel), th); 
 
 imgMask(imSeg > 2) = 1;
 
+% Eliminar pared a partir de paredMin
+depthTranslated = imtranslate(imgDepth ,[-8, 0],'FillValues',0);
+imgMask(depthTranslated(:,:,1) > paredMin) = 0;
+
+
 figure(8)
-subplot(1,2,1)
+subplot(2,2,1)
 imshow(imSeg, [])
-subplot(1,2,2)
+
+subplot(2,2,2)
 imshow(imgMask)
 
+subplot(2,2,3)
+imshow(imgDepth)
 
 %% Ajustar la mascara
-se = strel('disk', 2);
+se = strel('diamond', 3);
 
 imgMask = imdilate(imgMask, se);
 imgMask = imfill(imgMask, 'holes');
 imgMask = imerode(imgMask, se);
 
 imgMask = bwareaopen(imgMask, 1000);
-
-%imgMask = imtranslate(imgMask ,[-8, 0],'FillValues',0);
+subplot(2,2,4)
+imshow(imgMask)
 
 %% Mostrar resultados
 finalImg = bsxfun(@times, imgColor, cast(imgMask, 'like', imgColor));
@@ -52,7 +60,16 @@ figure(11)
 imshow(finalImg)
 hold on;
 % Las etiquetas se almacenan en colors
-colors = [195 64 63; 46 64 105; 122 58 21; 36 48 40];
+colors = [195 64 63; 46 64 105; 122 58 21; 36 48 40; 26, 8, 7];
+
+% Aplicar la mascara a la imagen en cielab
+finalLab = bsxfun(@times, imgLab, cast(imgMask, 'like', imgLab));
+% Variable auxiliar
+[u, v, ch] = size(imgColor);
+auxLab = zeros(3,u*v);
+for i = 1:3
+	auxLab(i,:) = reshape(finalLab(:,:,i), [u*v, 1]);		
+end
 
 % Convertir a cielab
 colorTransform = makecform('srgb2lab');
@@ -89,7 +106,7 @@ for i = 1:size(colors, 1)
         x = ceil(bBoxes(index).Centroid(1));
         y = ceil(bBoxes(index).Centroid(2));
 
-        rectangle('Position', bBoxes(index).BoundingBox, 'EdgeColor', [0.5 0.5 0.5], 'LineWidth', 1);
+        rectangle('Position', bBoxes(index).BoundingBox, 'EdgeColor', colors(i,:)./255, 'LineWidth', 0.5);
         text(x, y, 'Color' + string(i), 'Color', 'w', 'HorizontalAlignment', 'center');
     end
 end
